471bfef5a2e7a2406ac6be1a35e6bf83af1d79c6
KilliannH <killiannhervagault@gmail.com>
2020-05-03 17:12:43 +0200

    making mongo works again

--- mongo-init.js
+++ mongo-init.js
@@ -1,19 +1,11 @@
-const {
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME
-} = process.env;
-
-db = db.getSiblingDB(DB_NAME);
-
 db.createUser(
     {
-        user: DB_USER,
-        pwd: DB_PASSWORD,
+        user: "test-user",
+        pwd: "test-password",
         roles: [
             {
                 role: "readWrite",
-                db: DB_NAME
+                db: "mymusic-db"
             }
         ]
     }
