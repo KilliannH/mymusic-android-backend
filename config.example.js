4126a6663cd0afa3d57939aff3340e841c9cf677
KilliannH <killiannhervagault@gmail.com>
2020-11-15 22:26:36 +0100

    updated config example

--- config.example.js
+++ config.example.js
@@ -1,10 +1,12 @@
 const config = {
-	HOST: "localhost",
+	HOST: "0.0.0.0",
 	PORT: "3000",
 
 	DB_HOST: "mongo",
 	DB_PORT: "27017",
-	DB_NAME: "nativeMusic_db"
+	DB_NAME: "nativeMusic_db",
+
+	API_KEY: "secret"
 };
 
 module.exports = config;
